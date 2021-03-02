18ed780a478de5997249089812a70129962e2907
zlatakp <zlata1996karan@gmail.com>
2021-03-02 00:16:46 -0800

    Create Page mostly done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<str:title>/', views.title, name = "title")
+    path("newpage/", views.newpage, name = 'newpage'),
+    path("<str:title>/", views.title, name = 'title')
+    
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from . import models
 
 from . import util
-
+app_name = 'wiki'
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    """
+    Returns search results matching the query
+    """
+    if request.method == "POST":
+        q = request.POST.get("q")
+        all_entries = util.list_entries()
+        response = []
+        for entry in all_entries:
+            if entry.upper() == q.upper():
+                response = redirect(('/wiki/'+entry))
+            elif q.upper() in entry.upper():
+                response.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "results": response
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
+
 
 def title(request, title):
-    return render(request, "encyclopedia/title.html", {
-        "title": title,
-        "entry": util.get_entry(title)
-    })
+    entry = util.get_entry(title)
+    if entry == None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/title.html", {
+            "title": title,
+            "entry": entry
+        })
+
+def newpage(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        util.save_entry(title, content)
+    else:
+        return render(request, "encyclopedia/newpage.html", {
+            "form": models.EntryForm(auto_id = True)
+        })
+
+        
+
