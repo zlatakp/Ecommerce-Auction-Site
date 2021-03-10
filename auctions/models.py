79e4ce56adcd244f1d19a7f2115c04c7d3ec20b6
zlatakp <zlata1996karan@gmail.com>
2021-03-10 07:41:30 -0800

    restart

--- auctions/models.py
+++ auctions/models.py
@@ -4,18 +4,3 @@ from django.db import models
 
 class User(AbstractUser):
     pass
-
-class Listing(models.Model):
-    title = models.CharField(max_length = 100)
-    start_bid = models.IntegerField()
-    url = models.CharField(max_length = 200, blank = True)
-    description = models.CharField(max_length = 200, default = "")
-    def __str__(self):
-        return f"{self.title} starting at ${self.start_bid}."
-class Category(models.Model):
-    pass
-class Bid(models.Model):
-    pass
-
-class Comment(models.Model):
-    pass
