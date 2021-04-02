245c1218baa09d6905dddbc8ad7dd07a0cf1de8e
zlatakp <zlata1996karan@gmail.com>
2021-04-02 16:52:33 -0400

    watchlist css

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/login/", auth_views.LoginView.as_view()),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
--- auctions/views.py
+++ auctions/views.py
@@ -8,6 +8,24 @@ from .models import User, Category, Listing, Bid, Comment
 from datetime import datetime
 from .forms import NewListing, NewBid, NewComment
 
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "auctions/login.html")
 
 def index(request):
     if request.method == "POST":
@@ -172,24 +190,7 @@ def won(request):
     })
 
 
-def login_view(request):
-    if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "auctions/login.html")
 
 @login_required 
 def logout_view(request):
